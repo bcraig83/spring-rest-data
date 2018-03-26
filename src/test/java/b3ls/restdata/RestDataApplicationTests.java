d8b4357123e4e75f7672cdaef820d0be219af8a8
Ben Craig <bcraig83@gmail.com>
2018-03-26 21:22:28 +0100

    Initial skeleton code put in place

---
+++ src/test/java/b3ls/restdata/RestDataApplicationTests.java
@@ -0,0 +1,16 @@
+package b3ls.restdata;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RestDataApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
